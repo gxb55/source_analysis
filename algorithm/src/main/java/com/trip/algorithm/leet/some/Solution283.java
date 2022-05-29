d928ff7bb658f43728f780e87df7ae26c9a65640
guoxiaobing <guoxiaobing@lvmama.com>
2022-05-29 21:34:07 +0800

    leetcode

---
+++ algorithm/src/main/java/com/trip/algorithm/leet/some/Solution283.java
@@ -0,0 +1,82 @@
+package com.trip.algorithm.leet.some;
+
+import java.util.Arrays;
+
+/**
+ * @author xbguo
+ * @createTime 2022年05月29日 16:13:00
+ * 283. 移动零
+ * 给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序。
+ * <p>
+ * 请注意 ，必须在不复制数组的情况下原地对数组进行操作。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: nums = [0,1,0,3,12]
+ * 输出: [1,3,12,0,0]
+ * 示例 2:
+ * <p>
+ * 输入: nums = [0]
+ * 输出: [0]
+ * <p>
+ * <p>
+ * 提示:
+ * <p>
+ * 1 <= nums.length <= 104
+ * -231 <= nums[i] <= 231 - 1
+ * <p>
+ * <p>
+ * 进阶：你能尽量减少完成的操作次数吗？
+ * <p>
+ * 通过次数758,424提交次数1,185,429
+ */
+public class Solution283 {
+    public static void main(String[] args) {
+        Solution283 solution283 = new Solution283();
+        int[] nums = {1, 1, 0, 0, 12, 12, 23};
+        solution283.moveZeroes(nums);
+        System.out.println(Arrays.toString(nums));
+    }
+
+    public void moveZeroes(int[] nums) {
+        int x = -1;
+        int y = -1;
+        int t = -1;
+        for (int i = 0; i < nums.length; i++) {
+            int val = nums[i];
+            if (val == 0 && x == -1) {
+                x = i;
+                t = i;
+            }
+            if (val != 0) {
+                y = i;
+            }
+
+            if (x != -1 && y != -1 && y > x) {
+                swap(x, y, nums);
+                x = y;
+                for (int j = x + 1; j < nums.length; j++) {
+                    if (nums[j] != 0) {
+                        y = j;
+
+                        swap(x, y, nums);
+                        x = y;
+                    }
+                }
+                x = y = -1;
+                i = t;
+            }
+
+        }
+    }
+
+
+
+    private void swap(int x, int y, int[] nums) {
+        int t = nums[x];
+        nums[x] = nums[y];
+        nums[y] = t;
+    }
+}
---
+++ algorithm/src/main/java/com/trip/algorithm/leet/some/Solution738.java
@@ -0,0 +1,113 @@
+package com.trip.algorithm.leet.some;
+
+/**
+ * @author xbguo
+ * @createTime 2022年05月29日 11:34:00
+ * 738. 单调递增的数字
+ * 当且仅当每个相邻位数上的数字 x 和 y 满足 x <= y 时，我们称这个整数是单调递增的。
+ * <p>
+ * 给定一个整数 n ，返回 小于或等于 n 的最大数字，且数字呈 单调递增 。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: n = 10
+ * 输出: 9
+ * 示例 2:
+ * <p>
+ * 输入: n = 1234
+ * 输出: 1234
+ * 示例 3:
+ * <p>
+ * 输入: n = 332
+ * 输出: 299
+ * <p>
+ * <p>
+ * 提示:
+ * <p>
+ * 0 <= n <= 109
+ * 通过次数59,701提交次数119,345
+ */
+public class Solution738 {
+    public static void main(String[] args) {
+        Solution738 solution738 = new Solution738();
+        int val = 399443;
+        int i = solution738.monotoneIncreasingDigits(val);
+        System.out.println(i);
+    }
+
+    public int monotoneIncreasingDigits(int n) {
+        String str = n + "";
+        if (str.length() == 1) {
+            return n;
+        }
+        int index = getIndex(str);
+        if (index == -1) {
+            return n;
+        }
+        int len = str.length();
+        StringBuilder stringBuilder = new StringBuilder();
+
+        while (index >= 0) {
+            int left = index - 1;
+            if (left < 0) {
+                len = len - 1;
+                while (len > 0) {
+                    stringBuilder.append(9);
+                    len--;
+                }
+                return Integer.valueOf(stringBuilder.toString());
+            }
+            char c = str.charAt(index);
+            char c1 = str.charAt(left);
+            if (check(str, (char) (c - 1), index)) {
+                stringBuilder.append(str, 0, index).append((c - '1'));
+                len = len - stringBuilder.length();
+                while (len > 0) {
+                    stringBuilder.append(9);
+                    len--;
+                }
+                return Integer.valueOf(stringBuilder.toString());
+            }
+            if (left == 0 && c1 > '1') {
+                stringBuilder.append(str, 0, left).append((c1 - '1'));
+                while (len > 1) {
+                    stringBuilder.append(9);
+                    len--;
+                }
+                return Integer.valueOf(stringBuilder.toString());
+            }
+            index--;
+        }
+        return Integer.valueOf(stringBuilder.toString());
+    }
+
+    private boolean check(String str, char c1, int left) {
+        if (left == 0 && c1 == '0') {
+            return false;
+        }
+        char[] chars = str.toCharArray();
+        for (int i = 0; i < left; i++) {
+            char aChar1 = chars[i];
+            if (c1 < aChar1) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private int getIndex(String str) {
+        char[] chars = str.toCharArray();
+        char temp = chars[0];
+        for (int i = 1; i < chars.length; i++) {
+            char aChar1 = chars[i];
+            if (temp <= aChar1) {
+                temp = aChar1;
+            } else {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
