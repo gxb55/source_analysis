84a93030b5844460e66d931e6950f1f3a174a8f4
xbguo <xbguo@trip.com>
2023-02-07 20:39:08 +0800

    新增mvc项目

---
+++ spring_mvc_solve/src/main/java/com/trip/springmvc/Main.java
@@ -0,0 +1,11 @@
+package com.trip.springmvc;
+
+/**
+ * @author xbguo
+ * @date 2023/2/7 20:24
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(1);
+    }
+}
