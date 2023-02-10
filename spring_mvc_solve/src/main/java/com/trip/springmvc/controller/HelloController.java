683683469f7d22fd618d569111bd8b1f4004aa79
xbguo <xbguo@trip.com>
2023-02-10 18:23:06 +0800

    mvc day02

--- spring_mvc_solve/src/main/java/com/trip/springmvc/controller/HelloController.java
+++ spring_mvc_solve/src/main/java/com/trip/springmvc/controller/HelloController.java
@@ -2,23 +2,36 @@ package com.trip.springmvc.controller;
 
 import com.trip.springmvc.service.HelloService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpSession;
 
 /**
  * @author xbguo
  * @createTime 2023年02月07日 22:07:00
  */
-@RestController
+//@RestController
+@Controller
 public class HelloController {
 
     @Autowired
     HelloService helloService;
 
+    /**
+     * https://docs.spring.io/spring-framework/docs/5.3.25/reference/html/web.html#mvc-ann-arguments 能够处理的方法类型 方法签名
+     * <p>
+     * https://docs.spring.io/spring-framework/docs/5.3.25/reference/html/web.html#mvc-ann-return-types 能够处理的方法返回值
+     * Method Arguments
+     *
+     * @return
+     */
     @GetMapping("/hello")
-    public String sayHello() {
-        String mvc = helloService.sayHello("MVC");
-        return mvc;
+    public String sayHello(String name, @RequestParam("user") String user, HttpSession httpSession) {
+        String mvc = helloService.sayHello(user + "MVC" + name);
+        httpSession.setAttribute("msg", mvc);
+        return "redirect:index";
     }
 
     public HelloController() {
--- spring_mvc_solve/src/main/webapp/index.jsp
+++ spring_mvc_solve/src/main/webapp/index.jsp
@@ -1,5 +1,8 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <body>
 <h2>Hello World!</h2>
+<h1></h1>
+结果：${sessionScope.msg}
 </body>
 </html>
