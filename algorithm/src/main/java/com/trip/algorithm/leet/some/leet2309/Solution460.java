caf498820ca535d3d7cb1ea3dcd5881e1058f123
xbguo <xbguo@trip.com>
2023-10-07 16:52:27 +0800

    1007

---
+++ algorithm/src/main/java/com/trip/algorithm/leet/some/leet2309/Solution460.java
@@ -0,0 +1,13 @@
+package com.trip.algorithm.leet.some.leet2309;
+
+/**
+ * @author xbguo
+ * @date 2023/9/25 11:20
+ */
+public class Solution460 {
+    public static void main(String[] args) {
+
+    }
+}
+
+
