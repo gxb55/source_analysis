1ab33ca2f5c86acf30c9c83f329b338283c767af
xbguo <xbguo@trip.com>
2023-10-10 19:07:58 +0800

    231010

---
+++ my_start/src/main/java/com/example/my_start/MyStartApplication.java
@@ -0,0 +1,9 @@
+package com.example.my_start;
+
+
+public class MyStartApplication {
+
+    public static void main(String[] args) {
+    }
+
+}
---
+++ my_start/src/test/java/com/example/my_start/MyStartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.my_start;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyStartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
