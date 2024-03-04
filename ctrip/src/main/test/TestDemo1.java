94d96f0531b285972c9f663e6d7d1b199b50096c
gxb55 <1329860224@qq.com>
2024-03-04 21:57:14 +0800

    0304

---
+++ ctrip/src/main/test/TestDemo1.java
@@ -0,0 +1,11 @@
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+public class TestDemo1 {
+    @Test
+    public void testAdd() {
+        int a = 10;
+        int b = 10;
+        Assert.assertEquals(a + b, 30);
+    }
+}
---
+++ ctrip/src/main/test/com/train/ctrip/Test.java
@@ -0,0 +1,12 @@
+package com.train.ctrip;
+
+import org.junit.Assert;
+
+public class Test {
+    @org.junit.jupiter.api.Test
+    public void testAdd() {
+        int a = 10;
+        int b = 10;
+        Assert.assertEquals(a + b, 30);
+    }
+}
