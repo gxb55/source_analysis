4d28ba82369c784ca28f6cd4a702adeae28d0ac0
xbguo <xbguo@trip.com>
2024-05-23 19:36:01 +0800

    0523

---
+++ algorithm/src/main/java/com/trip/algorithm/leet/l24/l05/Solution2225.java
@@ -0,0 +1,7 @@
+package com.trip.algorithm.leet.l24.l05;/**
+ * @author xbguo
+ * @date 2024/5/23 16:43
+ * @description TODO
+ */
+public class Solution2225 {
+}
---
+++ algorithm/src/main/java/com/trip/algorithm/leet/l24/l05/Solution2831.java
@@ -0,0 +1,7 @@
+package com.trip.algorithm.leet.l24.l05;/**
+ * @author xbguo
+ * @date 2024/5/23 14:26
+ * @description TODO
+ */
+public class Solution2831 {
+}
